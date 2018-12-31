0d2a215099e105e68bb0f1262f65d7e9ec9a9413
[sky]quaker <skyquakerrr@gmail.com>
2018-12-31 15:05:38 -0500

    Simplify by removing renderer for now

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
 #include <stdio.h>
-#include "SDL.h"
+#include <SDL.h>
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    int rc = SDL_Init(SDL_INIT_VIDEO);
+    if (rc < 0) {
+        printf("SDL_Init failed (%s)\n", SDL_GetError());
+        return 1;
+    }
 
-    SDL_Window *window = SDL_CreateWindow(
+    SDL_Window* window = SDL_CreateWindow(
         "SDL2Test",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
         480,
-        0);
+        SDL_WINDOW_SHOWN);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    if (window == NULL) {
+        printf("SDL_CreateWindow failed (%s)\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0x00, 0x00));
+    SDL_UpdateWindowSurface(window);
 
     SDL_Delay(3000);
 
